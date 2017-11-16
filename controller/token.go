30c51e18911269103ebcf1cc0dff01e6919b7b8d
Alexey Kazakov <alkazako@redhat.com>
2017-11-16 09:39:44 -0800

    Delete account links from Keycloak

--- controller/token.go
+++ controller/token.go
@@ -208,6 +208,11 @@ func (c *TokenController) getKeycloakExternalTokenURL(providerName string) strin
 	return fmt.Sprintf("%s/auth/realms/%s/broker/%s/token", c.Configuration.GetKeycloakURL(), c.Configuration.GetKeycloakRealm(), providerName)
 }
 
+func (c *TokenController) getKeycloakIdentityProviderURL(identityID string, providerName string) string {
+	// not moving this to config because this is temporary.
+	return fmt.Sprintf("%s/auth/admin/realms/%s/users/%s/federated-identity/%s", c.Configuration.GetKeycloakURL(), c.Configuration.GetKeycloakRealm(), identityID, providerName)
+}
+
 // Retrieve fetches the stored external provider token.
 func (c *TokenController) Retrieve(ctx *app.RetrieveTokenContext) error {
 
@@ -224,7 +229,6 @@ func (c *TokenController) Retrieve(ctx *app.RetrieveTokenContext) error {
 
 	providerConfig, err := c.providerConfigFactory.NewOauthProvider(ctx, ctx.RequestData, ctx.For)
 	if err != nil {
-
 		return jsonapi.JSONErrorResponse(ctx, err)
 	}
 	providerName := providerConfig.TypeName()
@@ -281,17 +285,16 @@ func (c *TokenController) Delete(ctx *app.DeleteTokenContext) error {
 	}
 
 	// Delete from Keycloak
-		err = c.keycloakExternalTokenService.Delete(ctx, c.getKeycloakIdentityProviderURL(currentIdentity.String(), providerConfig.TypeName()))
+	err = c.keycloakExternalTokenService.Delete(ctx, c.getKeycloakIdentityProviderURL(currentIdentity.String(), providerConfig.TypeName()))
 	if err != nil {
-			log.Error(ctx, map[string]interface{}{
-				"error":         err,
-				"provider_name": providerConfig.TypeName(),
-				"identity_id":   currentIdentity,
-										  +		}, "Unable to remove Identity Provider link from Keycloak.")
-			// Not critical. Log the error and proceed.
-		}
-
-			// Delete from local DB
+		log.Error(ctx, map[string]interface{}{
+			"error":         err,
+			"provider_name": providerConfig.TypeName(),
+			"identity_id":   currentIdentity,
+		}, "Unable to remove Identity Provider link from Keycloak.")
+		// Not critical. Log the error and proceed.
+	}
+	// Delete from local DB
 
 	err = application.Transactional(c.db, func(appl application.Application) error {
 		err := appl.Identities().CheckExists(ctx, currentIdentity.String())
@@ -303,7 +306,12 @@ func (c *TokenController) Delete(ctx *app.DeleteTokenContext) error {
 			return err
 		}
 		if len(tokens) > 0 {
-			return appl.ExternalTokens().Delete(ctx, tokens[0].ID)
+			for _, token := range tokens {
+				err = appl.ExternalTokens().Delete(ctx, token.ID)
+				if err != nil {
+					return err
+				}
+			}
 		}
 		return nil
 	})
